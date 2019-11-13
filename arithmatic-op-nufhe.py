da8ea7e5083927a393980b988a01985184035b90
Napat <napat.charuphant1@gmail.com>
2019-11-13 23:57:35 +0700

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	new file:   .ipynb_checkpoints/bitwise-subtraction-playground-checkpoint.ipynb 	new file:   Bits-playground.ipynb 	modified:   arithmatic-op-nufhe.py 	new file:   bitwise-addition-playground.ipynb 	new file:   bitwise-subtraction-playground.ipynb

--- arithmatic-op-nufhe.py
+++ arithmatic-op-nufhe.py
@@ -7,22 +7,18 @@ def fixSizeBoolList(decimal,size):
     x = [False]*(size - len(x)) + x
     return x
 
+# in subtraction, ciX have to be greater than ciY
 def subtract(ciX, ciY):
     for i in range(size):
         ciXnotTemp = ciX
         a = vm.gate_and(vm.gate_not(ciX), ciY)
-        # print("a : ",ctx.decrypt(secret_key, a))
         ciX = vm.gate_xor(ciX, ciY)
-        # print("b : ",ctx.decrypt(secret_key, b))
         aShiftTemp = a
-        # print("aShiftTemp : ",ctx.decrypt(secret_key, aShiftTemp))
         aShiftTemp.roll(-1, axis=-1)
         ciY = aShiftTemp
-        # print("ciX : ",ctx.decrypt(secret_key, ciX))
-        # print("ciY : ",ctx.decrypt(secret_key, ciY))
+
     return ciX
 
-# incase the
 def checkSubtract(sub1,sub2):
     if sub1 > sub2:
         return sub2
@@ -32,19 +28,15 @@ def checkSubtract(sub1,sub2):
 def add(ciX, ciY):
     for i in range(size):
         a = vm.gate_and(ciX, ciY)
-        # print("a : ",ctx.decrypt(secret_key, a))
         b = vm.gate_xor(ciX, ciY)
-        # print("b : ",ctx.decrypt(secret_key, b))
         aShiftTemp = a
-        # print("aShiftTemp : ",ctx.decrypt(secret_key, aShiftTemp))
         aShiftTemp.roll(-1, axis=-1)
         ciX = aShiftTemp
-        # print("ciX : ",ctx.decrypt(secret_key, ciX))
         ciY = b
-        # print("ciY : ",ctx.decrypt(secret_key, ciY))
+
     return b
 
-def boolListToIntList(bitlists):
+def boolListToInt(bitlists):
     out = 0
     for bit in bitlists:
         out = (out << 1) | bit
@@ -54,7 +46,7 @@ ctx = nufhe.Context()
 secret_key, cloud_key = ctx.make_key_pair()
 # size even decimal only
 size = 32
-#
+# test decimal number
 deci_x = 3093
 deci_y = 1999
 
@@ -64,16 +56,15 @@ y = fixSizeBoolList(deci_y,size)
 print(y)
 ciX = ctx.encrypt(secret_key, x)
 ciY = ctx.encrypt(secret_key, y)
-# refAndGate = [(b1 and b2) for b1, b2 in zip(x, y)]
 vm = ctx.make_virtual_machine(cloud_key)
 
+# subtraction have to be done twice since we don't know which one is grater than the other
+subXthenY = ctx.decrypt(secret_key, subtract(ciX, ciY))
+subYthenX = ctx.decrypt(secret_key, subtract(ciY, ciX))
+# the Lesser result is the right one
+plainSubtractNumber = checkSubtract(boolListToInt(subXthenY),boolListToInt(subYthenX))
+print("reference subtract number is : ", (deci_x-deci_y) ,"/ nuFHE subtract number is : ", plainSubtractNumber)
 
-plainNumber = ctx.decrypt(secret_key, subtract(ciX, ciY))
-print("reference subtract number is : ", (deci_x-deci_y) ,"/ nuFHE subtract number is : ", boolListToIntList(plainNumber))
-# print("add nuber is... : ",ctx.decrypt(secret_key, add(ciX, ciY)))
 
-# a = vm.gate_not(ciY)
-# ciY.copy()
-# assert all(result_bits == refAndGate)
-# result = vm.gate_and(ciX, ciY)
-# result_bits = ctx.decrypt(secret_key, result)
+plainAddNumber = ctx.decrypt(secret_key, add(ciX, ciY))
+print("reference add number is : ", (deci_x+deci_y) ,"/ nuFHE subtract number is : ", boolListToInt(plainAddNumber))
